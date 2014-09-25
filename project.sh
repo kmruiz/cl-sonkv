7108225c46cd2a33b3b6cb8a0a4a8468065df958
kmruiz <masruizkevin@gmail.com>
2014-09-26 00:02:41 +0200

    added option to call a lisp-rule of project without starting the interactive environment

--- project.sh
+++ project.sh
@@ -1,3 +1,7 @@
 #/bin/sh
 
-sbcl --noinform --load "project.lisp"
+if [ X"$1" != X ]; then
+    sbcl --noinform --non-interactive --load "project.lisp" --eval "($1)"
+else
+    sbcl --noinform --load "project.lisp"
+fi
